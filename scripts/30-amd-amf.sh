e357430b22237a2c9f9440de83559bf68a2f14a7
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2022-01-08 10:37:32 +0100

    ci: Add AMD AMF v1.4.23

---
+++ scripts/30-amd-amf.sh
@@ -0,0 +1,14 @@
+REPOSITORY="https://github.com/GPUOpen-LibrariesAndSDKs/AMF.git"
+
+function amd_amf() {	
+	if [[ ${VERSION_MAJOR} -lt 4 ]]; then
+		exit 0
+	fi
+
+	git clone --depth 1 --branch "${AMDAMF_VERISON}" "${REPOSITORY}" /tmp/amd-amf
+	pushd /tmp/amd-amf > /dev/null
+	sudo cp -R amf/public/include/ /usr/${BUILD_PREFIX}/include/AMF
+	popd > /dev/null
+}
+
+amd_amf
